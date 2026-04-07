d491fdc5bb894629358541c73213fe8da16f1b1a
BogdanATA <batameev@gmail.com>
2026-04-07 15:11:27 -0400

    Declared 5 variables from step 1

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+        String favColor;
+        int startingYear;
+        char middleInitial;
+        boolean hasPets;
+        String niceMessage;
+
+
+    }
 }
