640ff29aa445061b6ead4bc4a62c2c71e13439a2
BogdanATA <batameev@gmail.com>
2026-04-07 16:14:20 -0400

    Added the logic for step 3

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -3,6 +3,13 @@ package com.pluralsight;
 public class Reciept {
     public static void main(String[] args) {
 
-        System.out.println();
+        String itemName = "apples";
+        double itemPrice = 2.31;
+
+        int quantity = 3;
+
+        double total = itemPrice * quantity;
+
+        System.out.println("You bought" + " " + quantity + " " + itemName + " " + "for $" + total);
     }
 }
